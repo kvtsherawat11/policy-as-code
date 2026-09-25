9456f46743a73b668632b22a647000fda18bb0fb
kvtsherawat11 <73883767+kvtsherawat11@users.noreply.github.com>
2026-09-25 19:04:55 +0530

    Add a simple HTTP server in app.py

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Policy as Code lab application is running!")
+
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+
+print("Application listening on port 8080")
+
+server.serve_forever()
